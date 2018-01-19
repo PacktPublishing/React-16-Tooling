0d43e30d23f9fbdbcfe05c2f1687db559d9c248f
Adam Boduch <adam.boduch@virtustream.com>
2018-01-19 15:47:05 -0500

    Chapter 6

--- 06/using-eslint-with-a-code-editor/src/App.js
+++ 06/using-eslint-with-a-code-editor/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img1 src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
